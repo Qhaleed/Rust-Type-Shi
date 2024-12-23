d895c98113b595de5932add95be6647c88b46ab2
Qhaleed <reslykadiri@gmail.com>
2024-12-24 02:04:15 +0800

    updit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 
+use core::num;
 use std::io;
 use rand::Rng;
 use std::io::{Write, BufRead, BufReader, ErrorKind};
@@ -107,28 +108,130 @@ use std::cmp::Ordering;
 
 // Arrays with RUST!!
 fn main () {
-    let arr_1 = [1,2,3,4,5];
-    println!("First element of the array: {}",arr_1.len());
-    let mut arr_1_index = 0;
-    while(arr_1_index != arr_1.len()) {
-        println!("{}", arr_1[arr_1_index]);
-        arr_1_index += 1;
-    }
+    // let arr_1 = [1,2,3,4,5];
+    // println!("First element of the array: {}",arr_1.len());
+    // let mut arr_1_index = 0;
+    // while(arr_1_index != arr_1.len()) {
+    //     println!("{}", arr_1[arr_1_index]);
+    //     arr_1_index += 1;
+    // }
 
-    let my_tuple: (u8,String, f64) = (47, "Res".to_string(), 4.0);
-    println!("Here is my tuple {}", my_tuple.1);
+    // let my_tuple: (u8,String, f64) = (47, "Res".to_string(), 4.0);
+    // println!("Here is my tuple {}", my_tuple.1);
 
 
-    // Strings with Rust
-     // There are 2 types of strings
-    // 1. String : Vector of bytes that can be changed
-    // 2. &str : Points to the string and allows for viewing
-    let mut st1 = String::new();
-    st1.push('a');
+    // // Strings with Rust
+    //  // There are 2 types of strings
+    // // 1. String : Vector of bytes that can be changed
+    // // 2. &str : Points to the string and allows for viewing
+    // let mut st1 = String::new();
+    // st1.push('a');
 
-    st1.push_str(" word");
+    // st1.push_str(" word");
 
-    for word in st1.split_whitespace(){
-        println!("{}", word);
-    }    
+    // for word in st1.split_whitespace(){
+    //     println!("{}", word);
+    // }    
+    // let arr_2 = [1,2,3,4,5,6,7,8,9];
+    // let mut loop_idx: usize = 0;
+    // loop {
+    //     if arr_2[loop_idx] %2 == 0 {
+    //         loop_idx +=1;
+    //         continue;
+    //     }
+    //     if  arr_2[loop_idx] > 9 {
+    //         break;
+    //     }
+    //     println!("Val : {}", arr_2[loop_idx]);
+    //     loop_idx += 1;
+    // }
+    // let mut user_name = "Res";
+    // println!("Hello {}!", user_name );
+    // println!("Please Enter your Age");
+
+    // let mut user_age = String::new();
+    // io::stdin().read_line(&mut user_age).expect("Failed to recieve line");
+
+    // println!("Name: {}", user_name);    
+    // println!("Age: {}", user_age.trim());    
+    
+//     let age:u32 = 30;
+//     let price:f64 = 99.99;
+//     let is_active = true;
+//     let initials: char = 'T';
+//     let message : &str = "Hello Rust!";
+//     let name = String::from("Resly Kadiri");
+
+//     println!("Name: {}", name);
+//     println!("Age: {}", age);
+//     println!("Black: {}", is_active);
+//     println!("Initials: {}", initials);
+//     println!("Message: {}", message);
+//     println!("Ratings: {}", price);
+
+
+//     // Try to write a small program that declares variables of different data types (integer, float, boolean, char, string)
+//     //  and prints their values to the console. This will help solidify your understanding of these basic types. 
+//     // Once you've attempted that, we can move on to more complex topics like control flow (if/else, loops) and ownership.
+
+//     let my_integer:i32 = 10;
+//     let my_float:f32 = 10.1;
+//     let my_bool = true;
+//     let my_char = 'r';
+//     let my_string = String::from("Res");
+
+//     for number in 1..4 {
+//         println!("{}", number);
+//     }
+//     println!("LIFTOFF!!!!!!!");
+
+//     let a = [1,2,3,4,5,6,7];
+
+//     for element in a.iter(){
+//         println!("Value is: {}", element);
+//     }
+
+//     // Write a program that takes an integer as input from the user. 
+//     // Use an if statement to check if the number is positive, negative, or zero. 
+//     // Print an appropriate message for each case.
+//     // Then, use a loop (either while or for) to print the numbers from 1 to the absolute value of the input number.
+
+//     println!("Give me a number");
+//     let mut user_number = String::new();
+//     io::stdin().read_line(&mut user_number)
+//     .unwrap();
+//     let trimmed_number = user_number.trim();
+//     let target_number:i32 = trimmed_number.parse().unwrap();
+//     println!("Your number is: {}", target_number);
+//     if target_number > 0 {
+//         println!("Number is positive");
+//     } else if target_number < 0 {
+//         println!("Number is negative");
+//     }
+//     else if target_number == 0{
+//         println!("Number is zero");
+//     }
+
+//     for numbers in 1..=target_number {
+//         println!("{}", numbers);
+//     }
+
+
+
+//     let s1 = String::from("Hello");
+//     let s2 = s1;
+
+//     println!("s1 = {}", s2);
+
+
+let s1 = String::from("Hello");
+let s2 = s1;
+
+println!("s1 = {}", s2);
+
+let mut s = String::from("hello");
+s = String::from("ahoy");
+
+println!("{s}, world!");
+
 }
